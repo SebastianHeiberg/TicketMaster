82d8f92f1164cc3a7688afbd047409280454708f
SebastianHeiberg <seba3673@stud.kea.dk>
2022-04-10 20:20:58 +0200

    basisopgave 1 done

---
+++ src/com/company/Event.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Event {
+
+  private String beskrivelse;
+  private int billetter;
+  private int billetpris;
+
+  public Event(String beskrivelse, int billetpris, int billetter){
+this.beskrivelse = beskrivelse;
+this.billetpris = billetpris;
+this.billetter = billetter;
+  }
+}
